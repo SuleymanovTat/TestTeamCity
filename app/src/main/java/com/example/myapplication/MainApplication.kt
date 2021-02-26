9e030e506f5b3980c8b0c07b85df7bc43a8445f9
suleymanovtat <suleymanovtat@gmail.com>
2021-02-26 22:20:27 +0300

    Firebase Remote Config

--- app/src/main/java/com/example/myapplication/MainApplication.kt
+++ app/src/main/java/com/example/myapplication/MainApplication.kt
@@ -1,6 +1,10 @@
 package com.example.myapplication
 
 import android.app.Application
+import android.util.Log
+import com.google.firebase.FirebaseApp
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 import com.onesignal.OSNotificationOpenedResult
 import com.onesignal.OSNotificationReceivedEvent
 import com.onesignal.OneSignal
@@ -9,6 +13,11 @@ import com.onesignal.OneSignal
 class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        initOneSignal()
+        initFirebaseRemoteConfig()
+    }
+
+    private fun initOneSignal() {
         // Enable verbose OneSignal logging to debug issues if needed.
         OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE)
         // OneSignal Initialization
@@ -37,7 +46,32 @@ class MainApplication : Application() {
         OneSignal.setLocationShared(false)
     }
 
+    private fun initFirebaseRemoteConfig() {
+        FirebaseApp.initializeApp(this)
+        FirebaseRemoteConfig.getInstance().apply {
+            //set this during development
+            val configSettings = FirebaseRemoteConfigSettings.Builder()
+                .setMinimumFetchIntervalInSeconds(0)
+                .build()
+            setConfigSettingsAsync(configSettings)
+            //set this during development
+
+            setDefaultsAsync(R.xml.remote_config_defaults)
+            fetchAndActivate().addOnCompleteListener { task ->
+                val updated = task.result
+                if (task.isSuccessful) {
+                    val updated = task.result
+                    Log.d(TAG, "Config params updated: $updated")
+                } else {
+                    Log.d(TAG, "Config params updated: $updated")
+                }
+            }
+        }
+
+    }
+
     companion object {
         private const val ONESIGNAL_APP_ID = "b4baf5ae-d362-48ec-b8c7-604e672a9ce6"
+        private const val TAG = "my"
     }
 }
--- app/src/main/java/com/example/myapplication/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/dashboard/DashboardViewModel.kt
@@ -3,11 +3,15 @@ package com.example.myapplication.ui.dashboard
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 
 class DashboardViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        val remoteConfig = FirebaseRemoteConfig.getInstance()
+        val isAuth = remoteConfig.getString("isAuth")
+        val message = remoteConfig.getString("title")
+        value = "isAuth: $isAuth\n$message"
     }
     val text: LiveData<String> = _text
 }
